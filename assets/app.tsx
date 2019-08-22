f137ca03f4dd23b7a6f19caa4c9167497012e680
Mark Biek <support@janustech.net>
2019-08-22 10:20:09 -0400

    fixup! WIP: typescript

--- assets/app.tsx
+++ assets/app.tsx
@@ -3,7 +3,13 @@ import { render } from 'react-dom';
 
 import App from './components/App';
 
+function test(name: string) {
+    console.log(`Hello ${name}`);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    test('Mark');
+
     let app = document.getElementById('app');
     if (app) {
         render(<App />, app);
